bf75243924d2b25a7046d9911e17128c2cb5e30c
Aayush-M-1 <aayush1mahobia1@gmail.com>
2024-07-03 14:07:52 +0530

    bug fixes

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -12,7 +12,6 @@ const NavBar = () => {
         <ul className="navbar-menu">
           <li onClick={() => setMenu("home")} className={menu==="home"?"active":""}>home</li>
           <li onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-          <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
           <li onClick={() => setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact-us</li>
         </ul>
         <div className="navbar-right">
