d057f15bfcd7b009d5f61e2c6ed69e4e0b056e48
Aayush-M-1 <aayush1mahobia1@gmail.com>
2024-07-14 18:42:30 +0530

    frontEnd completed

--- backend/controllers/order.controllers.js
+++ backend/controllers/order.controllers.js
@@ -76,4 +76,15 @@ const verifyOrder = async (req,res) => {
     }
 }
 
-export {placeOrder, verifyOrder}
+// user orders for frontend
+const userOrders = async (req,res) => {
+    try {
+        const orders = await Order.find({userId:req.body.userId});
+        res.json({success:true, data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"error while displaying user orders"})
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders}
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { placeOrder, verifyOrder } from '../controllers/order.controllers.js'
+import { placeOrder, userOrders, verifyOrder } from '../controllers/order.controllers.js'
 import authMiddleware from '../middlewares/auth.js'
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder)
 orderRouter.post("/verify", verifyOrder)
+orderRouter.post("/userorders", authMiddleware, userOrders)
 
 export default orderRouter
